545687474b6d1c05eda1144b0677b871b3a26271
Aleska Ortega <alebrave04@gmail.com>
2021-09-16 15:39:45 -0600

    testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
         + If a plane takes off, its `isFlying` property gets set to true.
         + If a plane lands, its `isFlying` property gets set to false.
 */
-
+// testing
 // EXAMPLE SOLUTION CODE:
 class Airplane {
   constructor(name) {
